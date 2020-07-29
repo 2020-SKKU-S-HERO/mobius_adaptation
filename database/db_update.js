046b3ca14a87717762050f3565c7ea64863d5bd4
hooN <johnhpark11@gmail.com>
2020-07-29 20:12:03 +0900

    test

--- database/db_update.js
+++ database/db_update.js
@@ -1,6 +1,7 @@
 let mysql = require('mysql');
 let os = require('os');
 const { sendAcksForNonConfirmablePackets } = require('coap/lib/parameters');
+const { min } = require('moment');
 
 const mobius_connInfo = {
     host: 'localhost',
@@ -23,31 +24,26 @@ const ourdb_connInfo = {
 global.writeDataToShero = function(data){
     let ourdb_connection = mysql.createConnection(ourdb_connInfo);
     let sql = '';
-    const hour = 3600000;
-    const minute = 60000;
-    const seconds = 1000;
-    let time, year, month, date, mil_second, hou, min, sec;
+   
+    let time, year, month, date, hou, min, sec, milsec;
 
     for(var i=0; i<data.length; i++){
         time = data[i].ri.split('-')[1];
         year = time.substring(0, 4);    month = time.substring(4, 6);
-        date = time.substring(6, 8);    mil_second = Number(time.substring(8, 17));
+        date = time.substring(6, 8);    hou = time.substring(8, 10);
+        min = time.substring(10, 12);   sec = time.substring(12, 14);
+        milsec = time.substring(14, 17);
 
-        hou = String(parseInt(mil_second / hour));  mil_second = mil_second % hour;
-        min = String(parseInt(mil_second / minute));    mil_second = mil_second % minute;
-        sec = parseInt(mil_second / seconds);   mil_second = mil_second % seconds;
-
-        if(hou.length==1) hou ='0'+hou;
-        if(min.length==1) min = '0'+min;
-
-        time = year + '-' + month + '-' + date + ' ' + hou + ':' + min + ':' + String(sec)
+        time = year + '-' + month + '-' + date + ' ' + hou + ':' + min + ':' + sec +'.' + milsec;
         sql = 'insert into co2_emissions(date_time,emissions,location) values('+'\''+ time + '\''+ ','+ String(data[0].con) + ',' + '\''+ String(data[0].cr) +'\'' + ')';
 
         ourdb_connection.query(sql, function(error, results, fields){
             if(error){
                 console.log('ERROR DETECTED ::::::::::::::::::::::', sql);
-            };
-            console.log('Successss::::::::::::::::::::::', sql);
+            }
+            else{
+                console.log('Successss::::::::::::::::::::::', sql);
+            }
         });
     }
 
@@ -56,40 +52,35 @@ global.writeDataToShero = function(data){
 
 global.getDataFromMobius = function(date){
     let mobius_connection = mysql.createConnection(mobius_connInfo);
-    const hour = 3600000;
-    const minute = 60000;
-    const seconds = 1000;
 
     date = Date(date)
     date = new Date(date);
 
-    year = date.getFullYear();  month = date.getMonth()+1;
-    day = date.getDate();   hou = date.getHours();
-    min = date.getMinutes();    sec = date.getMinutes();
-    milsec = date.getMilliseconds();
-
-    res_milsec = hou*hour + min*minute + sec*seconds + milsec;
-    res_milsec = String(res_milsec);
-    month = String(month);
-    day = String(day);
+    year = String(date.getFullYear());  month = String(date.getMonth()+1);
+    day = String(date.getDate());   hou = String(date.getHours());
+    minute = String(date.getMinutes());    sec = String(date.getSeconds());
+    milsec = String(date.getMilliseconds());
 
     if(month.length==1) month='0'+month;
     if(day.length==1) day='0'+day;
+    if(hou.length==1) hou='0'+hou;
+    if(minute.length==1) minute='0'+minute;
+    if(sec.length==1) sec='0'+sec;
+    if(milsec.length==1) milsec='00'+milsec;
+    else if(milsec.length==2) milsec='0'+milsec;
 
-    for(var i=res_milsec.length; i<9; i++){
-        res_milsec = '0'+res_milsec;
-    }
-
-    res = String(year)+month+day+res_milsec
+    res = year + month + day + hou + minute + sec + milsec;
     let sql = 'SELECT ri, con, cr FROM cin WHERE right(ri, 17) > '+ res;
 
     mobius_connection.query(sql, function(error, results, fields){
-        if(!results){
+        if(error){
             console.log('====================================== MOBIUS DATA NULL');
         }
-        console.log('SUCCESS on get Data From Mobius *****************************', sql);
-        console.log('----------------------------- DATA FROM MOIBUS : ', results);
-        writeDataToShero(results);
+        else{
+            console.log('SUCCESS on get Data From Mobius *****************************', sql);
+            console.log('----------------------------- DATA FROM MOIBUS : ', results);
+            writeDataToShero(results);
+        }
     });
     mobius_connection.end();
 };
@@ -100,11 +91,11 @@ exports.mobius_to_shero = function(){
     //ourdb_connection.connect();
 
     ourdb_connection.query('SELECT MAX(date_time) from co2_emissions', function(error, results, fields){
-        if(error) throw error;
-
-        console.log('===================== results : ', results);
-        getDataFromMobius(results);
-
+        if(error)throw error;
+        else{
+            console.log('===================== results : ', results);
+            getDataFromMobius(results);
+        }
     });
     ourdb_connection.end();
 };
