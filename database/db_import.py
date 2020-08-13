86c152452593b2fcd2ca58713e6204b4ef6ecb33
hooN <johnhpark11@gmail.com>
2020-08-13 13:51:21 +0900

    data import on DB from random csv data

---
+++ database/db_import.py
@@ -0,0 +1,35 @@
+
+import pandas as pd
+import pymysql.cursors
+
+data = pd.read_csv('testdata.csv', sep=',', header=0, names=['acid', 'caco3', 'co2'])
+
+columns = ['acid', 'caco3', 'co2']
+
+"""
+for index, row in data.iterrows():
+    print('Acid(ml) : ', row['acid'])
+    print('CaCO3(g) : ', row['caco3'])
+    print('CO2 : ', row['co2'])
+    print('index : ', index)
+"""
+
+testDB = pymysql.connect(
+    host = "localhost",
+    port = '3306',
+    user = "root",
+    password = 'shero',
+    database = 'test',
+    charset = 'utf8'
+)
+
+try:
+    cursor = testDB.cursor()
+    for index, row in data.iterrows():
+        sql = "INSERT INTO test (acid, caco3, co2) VALUES ("+
+            str(row['acid']) + "," +
+            str(row['caco3']) + "," +
+            str(row['co2']) + ")"
+    testDB.commit()
+finally:
+    testDB.close()
