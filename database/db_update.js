8e2b3d1437837ca12b7430b54bc9c58e8876bdc8
hooN <johnhpark11@gmail.com>
2020-09-01 20:45:23 +0900

    unite to KOREAN time

--- database/db_update.js
+++ database/db_update.js
@@ -21,70 +21,89 @@ const ourdb_connInfo = {
     multipleStatements: true
 }
 
-global.writeDataToShero = function(data){
+global.writeDataToShero = function (data) {
     let ourdb_connection = mysql.createConnection(ourdb_connInfo);
     let sql = '';
 
     let time, year, month, date, hou, min, sec, milsec, loc;
 
-    for(var i=0; i<data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         time = data[i].ri.split('-');
         info = time[0].split('/')[3];
         //console.log('4. inserted info (mobius -> shero): ', info);
 
         time = time[1];
-console.log('before time : ',time);
-        year = time.substring(0, 4);    month = time.substring(4, 6);
-        date = time.substring(6, 8);    hou = time.substring(8, 10);
-        min = time.substring(10, 12);   sec = time.substring(12, 14);
+        //console.log('before time : ', time);
+        year = time.substring(0, 4); month = time.substring(4, 6);
+        date = time.substring(6, 8); hou = time.substring(8, 10);
+        min = time.substring(10, 12); sec = time.substring(12, 14);
         milsec = time.substring(14, 17);
-        hou = parseInt(hou);
-        date = parseInt(date);
-        month = parseInt(month);
-        if(hou>=15){
-            hou = String(hou+9-24); //-9
-            if(date==31){
-                month = month+1;
-                date=1;
-                month = String(month);
-                date = String(date);
-            }
-            else
-                date = String(date+1);
-        }
-        else{
-            hou = String(hou+9); //-9
-            date = String(date);
-        }
 
-        time = year + '-' + month + '-' + date + ' ' + hou + ':' + min + ':' + sec +'.' + milsec;
-console.log('after time : ',time);
-        if(data[i].cr=='Sdongwon')
+        year = pareInt(year);   month = parseInt(month);
+        hou = parseInt(hou);    date = parseInt(date);
+        month = parseInt(month);
+
+        if (hou >= 15) {
+            hou = String(hou + 9 - 24); //-9
+            date = date + 1;
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+                if(date>31){
+                    date = 1;
+                    month = month + 1;
+                    if(month>12){
+                        month = 1
+                        year = year + 1;
+                    }
+                }
+            }    
+            else if(month == 4 || month == 6 || month == 9 || month == 11){
+                if(date>30){
+                    date = 1;
+                    month = month + 1;
+                }
+            }
+            else{
+                if(date>28){
+                    date = 1;
+                    month = month + 1;
+                }
+            }
+        }
+        else
+            hou = hou + 9; //-9
+        
+        year = String(year);    month = String(month);
+        hou = String(hou);  date = String(date);
+        month = String(month);
+
+        time = year + '-' + month + '-' + date + ' ' + hou + ':' + min + ':' + sec + '.' + milsec;
+        
+        if (data[i].cr == 'Sdongwon')
             loc = "인천";
-        else if(data[i].cr=='ShooN')
+        else if (data[i].cr == 'ShooN')
             loc = '병점';
         else
             loc = '수원';
 
-        if(info=='temp'){
-            sql = 'insert into temperature(date_time,temperature,location) values('+'\''+ time + '\''+ ','+ String(data[i].con) + ',' + '\''+ loc +'\'' + ')';
-		    //console.log(":::::::::: DB row temp is inserted in temp ");
+        if (info == 'temp') {
+            sql = 'insert into temperature(date_time,temperature,location) values(' + '\'' + time + '\'' + ',' + String(data[i].con) + ',' + '\'' + loc + '\'' + ')';
+            //console.log(":::::::::: DB row temp is inserted in temp ");
         }
-        else if(info=='flowRate'){
-            sql = 'insert into flow_velocity(date_time,flow_velocity,location) values('+'\''+ time + '\''+ ','+ String(data[i].con) + ',' + '\''+ loc +'\'' + ')';
-		    //console.log(":::::::::: DB row flowRate is inserted in flowRate ");
+        else if (info == 'flowRate') {
+            sql = 'insert into flow_velocity(date_time,flow_velocity,location) values(' + '\'' + time + '\'' + ',' + String(data[i].con) + ',' + '\'' + loc + '\'' + ')';
+            //console.log(":::::::::: DB row flowRate is inserted in flowRate ");
         }
-        else{
-            sql = 'insert into co2_emissions(date_time,emissions,location) values('+'\''+ time + '\''+ ','+ String(data[i].con) + ',' + '\''+ loc +'\'' + ')';
-		    //console.log(":::::::::: DB row co2 is inserted in co2 ");
+        else {
+            sql = 'insert into co2_emissions(date_time,emissions,location) values(' + '\'' + time + '\'' + ',' + String(data[i].con) + ',' + '\'' + loc + '\'' + ')';
+            //console.log(":::::::::: DB row co2 is inserted in co2 ");
         }
 
-        ourdb_connection.query(sql, function(error, results, fields){
-            if(error){
-              //  console.log('5. ERROR DETECTED when inserting info to sherdoDB', sql);
+        ourdb_connection.query(sql, function (error, results, fields) {
+            if (error) {
+                console.log('5. ERROR DETECTED when inserting info to sherdoDB', sql);
             }
-            else{
-                //console.log('5. Successfully inserted into sheroDB with sql :', sql);
+            else {
+                console.log('5. Successfully inserted into sheroDB with sql :', sql);
                 //console.log('');
             }
         });
@@ -93,88 +112,86 @@ console.log('after time : ',time);
     ourdb_connection.end();
 }
 
-global.getDataFromMobius = function(result){
+global.getDataFromMobius = function (result) {
     let mobius_connection = mysql.createConnection(mobius_connInfo);
 
     date = result[0].time
-  console.log('Maximum date from sherodb : ', date);
-  console.log('');
-
-  year = String(date.getFullYear());
-  minute = String(date.getMinutes());    sec = String(date.getSeconds());
-  milsec = String(date.getMilliseconds());
-
-  month = date.getMonth()+1;
-  day = date.getDate();
-  hou = date.getHours();
-  hou_c = 0;
-  if(date.getHours()<9){
-      hou_c = String(hou-9+24); //-9
-      if(day==1){
-          month = month-1;
-          if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month == 12)
-              day=31;
-          else if(month==4 || month==6 || month==9 || month==11)
-              day=30;
-          else
-              day=28;
-          month = String(month);
-          day = String(day);
-      }
-      else
-          day = String(day-1);
-
-  }
-  else{
-      hou_c = String(hou-9); //-9
-      day = String(day);
-  }
-    if(month.length==1) month='0'+month;
-    if(day.length==1) day='0'+day;
-    if(hou.length==1) hou='0'+hou;
-    if(minute.length==1) minute='0'+minute;
-    if(sec.length==1) sec='0'+sec;
-    if(milsec.length==1) milsec='00'+milsec;
-    else if(milsec.length==2) milsec='0'+milsec;
-
-    res = year + month + day + hou_c + minute + sec + milsec;
-    console.log('2. Converted date info (date_time -> 17 digit string) : ', res);
-    console.log('');
-    console.log(hou);
+    console.log('Maximum date from sherodb : ', date);
     console.log('');
 
-    let sql = 'SELECT ri, con, cr FROM cin WHERE right(ri, 17) > '+ res;
+    minute = String(date.getMinutes()); sec = String(date.getSeconds());
+    milsec = String(date.getMilliseconds());
 
-    mobius_connection.query(sql, function(error, results, fields){
-        if(!results){
+    year = date.getFullYear();  month = date.getMonth() + 1;
+    day = date.getDate();   hou = date.getHours();
+    hou = 0;
+
+    if (date.getHours() < 9) {
+        hou = String(hou - 9 + 24); //-9
+        if (day == 1) {
+            month = month - 1;
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10)
+                day = 31;
+            else if (month == 4 || month == 6 || month == 9 || month == 11)
+                day = 30;
+            else if (month == 0){
+                month = 12;
+                day = 31;
+            }
+            else
+                day = 28;
+            month = String(month);
+            day = String(day);
+        }
+        else
+            day = String(day - 1);
+    }
+    else {
+        hou = String(hou - 9); //-9
+        day = String(day);
+    }
+    if (month.length == 1) month = '0' + month;
+    if (day.length == 1) day = '0' + day;
+    if (hou.length == 1) hou = '0' + hou;
+    if (minute.length == 1) minute = '0' + minute;
+    if (sec.length == 1) sec = '0' + sec;
+    if (milsec.length == 1) milsec = '00' + milsec;
+    else if (milsec.length == 2) milsec = '0' + milsec;
+
+    res = year + month + day + hou + minute + sec + milsec;
+    //console.log('2. Converted date info (date_time -> 17 digit string) : ', res);
+    //console.log('');
+    
+    let sql = 'SELECT ri, con, cr FROM cin WHERE right(ri, 17) > ' + res;
+
+    mobius_connection.query(sql, function (error, results, fields) {
+        if (!results) {
             console.log('');
             console.log('3. No matched ri with date string');
         }
-        else{
+        else {
             //console.log('');
-            //console.log('SUCCESS on get Data From Mobius ********************', sql);
-            console.log('');
-            console.log('3. Date string matched result : ', results, 'and length : ',results.length);
-            console.log('');
+            //console.log('3. Date string matched result : ', results, 'and length : ', results.length);
+            //console.log('');
             //console.log('-------------------------------DATA LENGTH : ', results.length);
             //console.log('');
-            if(results.length > 0)
+            if (results.length > 0)
                 writeDataToShero(results);
         }
     });
     mobius_connection.end();
 };
 
-exports.mobius_to_shero = function(){
+exports.mobius_to_shero = function () {
     let ourdb_connection = mysql.createConnection(ourdb_connInfo);
     //mobius_connection.connect();
     //ourdb_connection.connect();
 
-    ourdb_connection.query('SELECT MAX(date_time) as time from co2_emissions', function(error, results, fields){
-        if(error)throw error;
-        else{
-            console.log('1. Getting Max(date_time) from existing SheroDB : ', results);
-            console.log('');
+    ourdb_connection.query('SELECT MAX(date_time) as time from co2_emissions', function (error, results, fields) {
+        if (error) throw error;
+        else {
+            //console.log('1. Getting Max(date_time) from existing SheroDB : ', results);
+            //console.log('');
             getDataFromMobius(results);
         }
     });
