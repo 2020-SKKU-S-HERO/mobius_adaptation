7ad8d56dacab94b8d99914f0fe1ee9ccda0c5d04
hooN <johnhpark11@gmail.com>
2020-09-04 14:55:23 +0900

    import data

--- database/data_import.py
+++ database/data_import.py
@@ -10,38 +10,23 @@ sheroDB = pymysql.connect(
 		charset = 'utf8'
 )
 
-sheet = ['인천', '수원', '병점']
+#sheet = ['인천', '수원', '병점']
 
-<<<<<<< HEAD
-try:
-	cursor = sheroDB.cursor()
-	for loc in sheet:
-		data = pd.read_excel('CO2_emission_datasheet.xlsx', sheet_name=loc, header=0,
-				names=['limestone', 'clay', 'silica_stone', 'iron_oxide', 'gypsum',
-				'coal', 'carbon_dioxide', 'date'])
-=======
 
 try:
     cursor = sheroDB.cursor()
-    for loc in sheet:
-        data = pd.read_excel('CO2_emission_datasheet.xlsx', sheet_name=loc,header=0,
-		    names=['limestone', 'clay', 'silica_stone', 'iron_oxide', 'gypsum', 'coal', 'carbon_dioxide', 'date'])
->>>>>>> 8810dc42fcb85f53d73ccc81afbd20406519e172
-		for index, row in data.iterrows():
-			sql = "INSERT INTO input (date_time, location,limestone, clay, silica_stone, iron_oxide, gypsum, coal) VALUES ('"+str(row['date'])+"','"+ loc +"',"+str(row['limestone']) + ","+str(row['clay'])+","+str(row['silica_stone'])+ "," + str(row['iron_oxide']) + "," + str(row['gypsum']) + "," + str(row['coal']) + ");"
-#print(sql)            
-			cursor.execute(sql)
-			sql = "INSERT INTO co2_emissions (date_time, location, emissions) VALUES ('"+str(row['date'])+"','"+loc +"',"+str(row['carbon_dioxide']) + ");"
-			cursor.execute(sql)
-<<<<<<< HEAD
-			sheroDB.commit()
-finally:
-	sheroDB.close()
-=======
-            sheroDB.commit()
+    #for loc in sheet:
+	data = pd.read_csv('dataset.csv', header=0,
+		    names=['date_time', 'limestone', 'clay', 'silica_stone', 'iron_oxide', 'gypsum', 'coal', 'carbon_dioxide'])
+	for index, row in data.iterrows():
+		sql = "INSERT INTO co2_emissions(date_time, location, emissions, limestone, clay, silica_stone, iron_oxide, gypsum, coal) VALUES (\'"+str(row['date_time'])+"\',병점,"+ str(row['carbon_dioxide'])+','+ str(row['limestone']) + ","+str(row['clay'])+","+str(row['silica_stone'])+ "," + str(row['iron_oxide']) + "," + str(row['gypsum']) + "," + str(row['coal']) + ");"+ "ON DUPLICATE KEY UPDATE(emissions="+str(row['carbon_dioxide'])+", limestone="+str(row['limestone'])+", clay="+str(row['clay'])+", silica_stone="+str(row['silica_stone'])+", iron_oxide="+ str(row['iron_oxide'])+", gypsum="+str(row['gypsum'])+", coal="+str(row['coal'])+");"
+	#print(sql)            
+		cursor.execute(sql)
+			#sql = "INSERT INTO co2_emissions (date_time, location, emissions) VALUES ('"+str(row['date'])+"','"+loc +"',"+str(row['carbon_dioxide']) + ");"
+			#cursor.execute(sql)
+    sheroDB.commit()
 finally:
     sheroDB.close()
->>>>>>> 8810dc42fcb85f53d73ccc81afbd20406519e172
 """
 	for index, row in data.iterrows():
 		print('Limestone : ', row['limestone'])
