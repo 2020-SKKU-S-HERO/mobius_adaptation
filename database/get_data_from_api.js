9fcec89cec4a6a98e5ac9b3e45b1ebf35a30da38
hooN <johnhpark11@gmail.com>
2020-10-19 05:23:18 +0900

    temp

--- database/get_data_from_api.js
+++ database/get_data_from_api.js
@@ -1,12 +1,15 @@
 const request = require('request');
 
-const host = 'ecos.bok.or.kr/api/StatisticSearch'
+const host = 'http://ecos.bok.or.kr/api/StatisticSearch'
 const CODE_exchange_rate = '036Y001';
 const dollar = '0000001';
 const token = 'TV0P8YV3A6NS778F0JIZ';
 
-let url = host+'/'+token+'/json/kr/'+'1/'+'10/'+CODE_exchange_rate+'/DD/'+'20200901/'+'20200910/'+dollar;
+let url = host+'/'+token+'/json/kr/'+'1/'+'1/'+CODE_exchange_rate+'/DD/'+'20200901/'+'20200901/'+dollar;
+
+console.log(url);
 
 request(url, function(err, res, body){
-    console.log(res);
+    //console.log(res);
+	console.log(body);
 })
