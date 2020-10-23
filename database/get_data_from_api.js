3793668c7b64956ac501ece0b03a8c4e4aa34af1
hooN <johnhpark11@gmail.com>
2020-10-23 23:13:42 +0900

    get electricity data

--- database/get_data_from_api.js
+++ database/get_data_from_api.js
@@ -16,26 +16,43 @@ const db_connInfo = {
 	multipleStatements: true
 };
 
-global.get_various_data = function(){
-	
-	let today = new Date();
-	let today_date = String(today.getFullYear())+ String(today.getMonth()+1)+ String(today.getDate());
+let today = new Date();
+let today_date = String(today.getFullYear())+ String(today.getMonth()+1)+ String(today.getDate());
+
+
+const ep_host = 'https://openapi.kpx.or.kr/openapi/forecast1dMaxBaseDate/';
+const ep_CODE = 'getForecast1dMaxBaseDate';
+const ep_CODE_today = 'Sukub5mToday';
+const ep_serviceKey = 'VbQNuye6GhNGAQ00jwTa6eGyuFnBY%2B9bmGMYEVXsqBY94ARYroVyHA0WxwOse6em%2FdhcrPZUWBvsoCgl6sDrww%3D%3D';
+
+global.electricity_power = function(){
+	let url = ep_host + ep_CODE_today + '?serviceKey=' + ep_serviceKey;
+	console.log(url);
+	let par;
+	request(url, function(err, res, body){
+		console.log(res);
+		console.log(body);
+		par = JSON.parse(body);
+		console.log(par);
+	});
+}
+
+//환율 정보를 API로부터 받아 DB에 쓴다.
+global.exchange_rate = function(){	
 
 	let url = er_host+'/'+er_token+'/json/kr/'+'1/'+'1/'+er_CODE+'/DD/'+today_date + '/'+today_date+ + '/'+er_dollar;
 	let par;
 
 	request(url, function(err, res, body){
 		par = JSON.parse(body);
-		writeData(par.StatisticSearch.row[0]);
+		writeData(par.StatisticSearch.row[0], 'exchage_rate');
 	});
 }
 
-global.writeData = function(data){
-
+//DB에 쓰는 부분이다.
+global.writeData = function(data, table_name){
 	const db_connection = mysql.createConnection(db_connInfo);
-
-	let sql = 'INSERT INTO exchange_rate(date_time, dollar) VALUES (';
-	sql = sql + data.TIME;
+	let sql = 'INSERT INTO '+ table_name+'(date_time, dollar) VALUES ('+data.TIME;
 	sql = sql + ',' + data.DATA_VALUE + ');';
 
 	db_connection.query(sql, function(error, results, fields){
@@ -47,4 +64,5 @@ global.writeData = function(data){
 	});
 }
 
-get_various_data();
+electricity_power();
+//exchange_rate();
